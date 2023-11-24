cc1df7dada0e6134df4aa1598baf303812e63d24
Leonardo Prodossimo <leohp14@hotmail.com>
2023-11-23 21:19:06 -0300

    att

--- ED_2PSI_2023/2BIM/Aula005_23_11_23/EX001.py
+++ ED_2PSI_2023/2BIM/Aula005_23_11_23/EX001.py
@@ -1,5 +1,5 @@
-N = 3
-M = 3
+N = 5
+M = 5
 
 import random as r
 matriz = []
@@ -9,17 +9,16 @@ for i in range(N):
     for j in range(M):
         num  = r.randint(1,100)
         lista.append(num)
-        print(lista)
     matriz.append(lista)
-
-print(matriz)
 
 print("------")
 qtd = 0
 for i in range(N):
     for j in range(M):
+        print(matriz[i][j],end='\t')
         if(matriz[i][j] % 2 == 0):
-            print(matriz[i][j],end='\t')
             qtd += 1
+    print()
 print()
 print(f"Quantidade de números pares é: {qtd}")
+print()
